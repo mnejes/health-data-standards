9ebc6f08ce7a8814ef20a3f16f12b63efa4391cb
Nejes, Michael <Michael_Nejes@premierinc.com>
2019-03-21 23:55:35 -0400

    Fix for rson codes

--- lib/health-data-standards/import/cda/section_importer.rb
+++ lib/health-data-standards/import/cda/section_importer.rb
@@ -185,7 +185,7 @@ module HealthDataStandards
         # coded_parent_element is the 'parent' element when the coded is nested (e.g., medication order)
         def extract_reason_or_negation(parent_element, entry, coded_parent_element = nil)
           coded_parent_element ||= parent_element
-          reason_element = parent_element.at_xpath("./cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.88']/cda:value | ./cda:entryRelationship[@typeCode='RSON']/cda:act[cda:templateId/@root='2.16.840.1.113883.10.20.1.27']/cda:code")
+          reason_element = parent_element.at_xpath("./cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.88']/cda:value | ./cda:entryRelationship[@typeCode='RSON']/cda:act[cda:templateId/@root='2.16.840.1.113883.10.20.1.27']/cda:code | ./cda:entryRelationship[@typeCode='SUBJ']/cda:supply/cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.88']/cda:value")
           negation_indicator = parent_element['negationInd']
           if reason_element
             code_system_oid = reason_element['codeSystem']
